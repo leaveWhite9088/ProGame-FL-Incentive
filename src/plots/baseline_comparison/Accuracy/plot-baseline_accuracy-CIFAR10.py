85cf9df2229177da9a91ebfa96b63adf5fcebadc
leaveWhite9088 <1411169639@qq.com>
2025-09-02 18:49:44 +0800

    update plots

--- src/plots/baseline_comparison/Accuracy/plot-baseline_accuracy-CIFAR10.py
+++ src/plots/baseline_comparison/Accuracy/plot-baseline_accuracy-CIFAR10.py
@@ -11,16 +11,16 @@ users = range(100)
 
 utility_baseline =
 utility_pgirdfl =
-utility_fair_flearn =
+utility_fixed =
+utility_random =
 utility_FL_Client_Sampling =
-utility_Oort =
 
 # 绘制图表
 plt.plot(users, utility_baseline, 'r--o', label='Baseline')  # 红色虚线，圆形标记
 plt.plot(users, utility_pgirdfl, 'b--s', label='PGI-RDFL')  # 蓝色虚线，方形标记
-plt.plot(users, utility_fair_flearn, 'y--o', label='Fair-FLearn')
+plt.plot(users, utility_fixed, 'y--o', label='Fixed')
+plt.plot(users, utility_random, 'g--^', label='Random')
 plt.plot(users, utility_FL_Client_Sampling, 'g--^', label='Client-Sampling')
-plt.plot(users, utility_Oort, 'g--^', label='Oort')
 
 # 添加图例
 plt.legend()
